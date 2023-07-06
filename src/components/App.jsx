c1c0873e9405169c609c983ac014b8bb5eb084be
Denys Tryfonov <trifonowden123@gmail.com>
2023-07-06 18:01:24 +0300

    try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Phonebook } from './Phonebook/Phonebook'
+import { Component } from "react";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  render() {
+      return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <Phonebook/>
+      </div>
+    )
+  }
+}
