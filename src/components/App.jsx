7728c00a4f7aec4c666cb038ee6092e95de9345d
Denys Tryfonov <trifonowden123@gmail.com>
2023-07-07 18:00:20 +0300

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ export class App extends Component {
       <div
         style={{
           height: '100vh',
-          display: 'flex',
+          // display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           fontSize: 40,
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,18 +1,41 @@
 import { nanoid } from 'nanoid'
+import { Formik, Form, Field } from 'formik'
+import styled from 'styled-components'
+
+const initialValues = {
+  name: ''
+}
+
+const Input = styled(Field)`
+color: #2a2a2a
+`
 
 export const Phonebook = () => {
-
-    return (
-      <form action="">
-        <input
-        data={nanoid()}
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-        />
-        <button>Add contact</button>
-      </form>
+  const handleSubmit = (values, {resetForm}) => {
+    console.log(values);
+    console.log(initialValues.name);
+    resetForm()
+  }
+  return (
+    <>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}>
+      <Form autoComplete='off'>
+        <label htmlFor="name">Name</label>
+          <Field
+          data={nanoid()}
+          type="text"
+          name="name"
+          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          />
+          <button type='submit'>Add contact</button>
+        </Form>
+      </Formik>
+      <div>Contact</div>
+      <div></div>
+    </>
     )
   }
